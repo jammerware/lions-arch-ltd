bc9734dfc473eaa25ba86a2a7442c8c390a03e03
Ben Stein <Ben Stein>
2016-10-27 07:41:27 -0700

    Fixed a bug in the crappy markdown pipe. Added a logo file, because logos are good.

--- app/shared/pipes/markdown.pipe.ts
+++ app/shared/pipes/markdown.pipe.ts
@@ -2,7 +2,6 @@ import { Inject, Pipe, PipeTransform } from '@angular/core';
 
 // this is going to be a pretty ugly implementation of this, but it turns out that parsing markdown is quite difficult.
 // at least, if you're me. but hey, it's modular, so we can fix it later.
-// TODO: consider security ramifications of this use of DomSanitiezr
 @Pipe({name: 'markdown'})
 export class MarkdownPipe implements PipeTransform {
   transform(markdown: string): string {
@@ -17,7 +16,7 @@ export class MarkdownPipe implements PipeTransform {
   }
 
   replaceLinkSyntax(markdown: string): string {
-    return markdown.replace(/\[([\s\S]+)\]\((\S+)\)/, function(match, group1, group2) {
+    return markdown.replace(/\[([^\]]+)\]\((\S+)\)/, function(match, group1, group2) {
       return `<a rel="noopener noreferrer" target="_blank" href="${group2}">${group1}</a>`;
     });
   }
