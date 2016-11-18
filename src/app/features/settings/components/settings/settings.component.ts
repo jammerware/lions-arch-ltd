0dac1d7b2458ec2c8b93d298250aca84af5e614e
ben <ben@jammerware.com>
2016-11-17 21:44:43 -0500

    Added a stub settings feature.

---
+++ src/app/features/settings/components/settings/settings.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'lal-settings',
+    templateUrl: 'settings.component.html'
+})
+export class SettingsComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
---
+++ src/app/features/settings/settings.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+
+import { SettingsComponent }   from './settings.component';
+
+@NgModule({
+    declarations: [SettingsComponent],
+})
+export class SettingsModule { }
