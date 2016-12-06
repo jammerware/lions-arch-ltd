ece18ef863276a6511e717a54c279641dcf8a557
ben <ben@jammerware.com>
2016-12-05 22:44:24 -0500

    Added manual references to _vars.scss in appropriate files as a workaround for the sass problem for now.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,24 +5,24 @@ import { Timespan } from './modules/timespan/timespan';
 import { EventNotificationsService } from './core/services/event-notifications-service/event-notifications.service';
 
 @Component({
-  selector: 'lal-app',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+    selector: 'lal-app',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  private title: string = `Lion's Arch, Ltd.`;
-  private upcomingEventsSub: Subscription;
+    private title: string = `Lion's Arch, Ltd.`;
+    private upcomingEventsSub: Subscription;
 
-  constructor(private eventNotificationsService: EventNotificationsService) { }
+    constructor(private eventNotificationsService: EventNotificationsService) { }
 
-  ngOnInit(): void {
-    // kick off the thing that listens for upcoming events and ticks every 10 seconds or whatever
-    this.upcomingEventsSub = this.eventNotificationsService
-      .getUpcomingEvents(Timespan.fromMinutes(10).totalMilliseconds, 15000)
-      .subscribe();
-  }
+    ngOnInit(): void {
+        // kick off the thing that listens for upcoming events and ticks every 10 seconds or whatever
+        this.upcomingEventsSub = this.eventNotificationsService
+            .getUpcomingEvents(Timespan.fromMinutes(10).totalMilliseconds, 15000)
+            .subscribe();
+    }
 
-  ngOnDestroy(): void {
-    if (this.upcomingEventsSub) { this.upcomingEventsSub.unsubscribe(); }
-  }
+    ngOnDestroy(): void {
+        if (this.upcomingEventsSub) { this.upcomingEventsSub.unsubscribe(); }
+    }
 }
